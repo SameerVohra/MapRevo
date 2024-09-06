393fecc15ccae43c3dae12e0f0bb4ab2a2c1ccf5
SameerVohra <sameer6002.be21@chitkara.edu.in>
2024-09-06 10:26:01 +0530

    fixed some unknown errors

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,10 @@
-import { Outlet } from 'react-router'
+import { Navigate, Outlet } from 'react-router'
 import './App.css'
 
 function App() {
   return (
     <>
+      <Navigate to="/login"/>
       <Outlet/>
     </>
   )
